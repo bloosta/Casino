a3694cef96a7b980194196e39ea349492a44f5f8
bloosta <85750448+bloosta@users.noreply.github.com>
2025-02-24 13:20:06 +0300

    Classes
    Add classes "Client" and "Session"

--- CasinoConsoleApp/Client.cs
+++ CasinoConsoleApp/Client.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace CasinoConsoleApp
 {
-    class Author
+    public class Client
     {
-        .
+        public int Id { get; set; }
+        public string Name { get; set; }
+
     }
 }
